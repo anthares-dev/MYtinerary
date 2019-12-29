de1b0a4dc8a9515a55e1ed93c999eade584f4c94
Fulvio_Vigilante <fulvio.vigilante@gmail.com>
2019-12-29 14:45:05 +0100

    serviceWorker to unregister

--- client/src/index.js
+++ client/src/index.js
@@ -88,8 +88,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
 
+/*
 //* Add To Homescreen
 var deferredPrompt;
 
@@ -124,3 +125,4 @@ function addToHomeScreen() {
     deferredPrompt = null;
   });
 }
+*/
